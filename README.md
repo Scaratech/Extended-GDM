7c3631a8db62ca6ad7746ea2e67f9d9d61ceee2c
Scaratek <153395462+Entrpix@users.noreply.github.com>
2024-11-02 18:55:04 -0400

    added image support

--- README.md
+++ README.md
@@ -1,30 +1,23 @@
 # Extended-GDM
+
 Extended-GDM is a Discord Selfbot for bridging one Group DM to another (Since Discord has a dumb limit of 10 people per Group DM)
 
 ## How to setup
+
 ```sh
-$ git clone https://github.com/scaratek/extended-gdm
-$ cd extended-gdm
-$ npm i
+git clone https://github.com/scaratek/extended-gdm
+cd extended-gdm
+npm i
 ```
 
-Inside of the `node_modules` directory look for the file `PartialGroupDMChannel.js` inside of `discord.js-selfbot`, `src`, `structures`
-1. Replace line 3, `const Channel = require('./Channel')` with `const Channel = require('./DMChannel')`
+Inside of the `node_modules` directory, find the file `PartialGroupDMChannel.js` inside of `discord.js-selfbot/src/structures` and modify line 3 to be `const Channel = require('./DMChannel');`\
+Then create a file called config.json and paste in:
 
-Then create a file called `.env` and paste in `TOKEN=`, after the equal sign put in your Discord Token
-
-Then create a filed called `config.json` and paste in this:
 ```json
 {
-   "groupIDs": ["Group DM ID 1", "Group DM ID 2"]
+    "token": "Your Discord Token",
+    "groupIDs": ["Group DM ID #1", "Group DM ID #2"]
 }
 ```
 
-Replace them with the actual IDs of the Group DMs you want to bridge. You can find them by enabling "Developer Mode" under "Advanced" in the settings and then right click on the Group ID and click "Copy Channel ID".
-
-Then just run `node index.js`!
-
-## TODO
-- [ ] Image/file support
-- [ ] Potentially bridging more than just two Group DMs
-- [ ] Automatic setup for skids
+Then run `node index.js`
--- index.js
+++ index.js
@@ -1,36 +1,60 @@
-const Discord = require('discord.js-selfbot');
-require('dotenv').config();
+import Discord from 'discord.js-selfbot';
+import fs from 'node:fs';
 
-const config = require('./config.json');
+const config = JSON.parse(fs.readFileSync('config.json'));
 
 const client = new Discord.Client();
 
-let gdmOne;
-let gdmTwo;
+let gdmOne, gdmTwo;
 
 client.once('ready', async () => {
     console.log('[E-GDM] Loaded');
 
     gdmOne = await client.channels.fetch(config.groupIDs[0]);
     gdmTwo = await client.channels.fetch(config.groupIDs[1]);
+
+    await gdmOne.send('[E-GDM] Loaded') &&  await gdmTwo.send('[E-GDM] Loaded');
 });
 
-client.on('message', (message) => {
+client.on('message', async (message) => {
     if (message.content.startsWith('[E-GDM]')) {
         return;
     }
 
     if (message.channel.id == config.groupIDs[0]) {
-        if (gdmTwo && gdmTwo.send) {
-            gdmTwo.send(`[E-GDM] ${message.author.username}: ${message.content}`);
+        const forwardedMessage = await gdmTwo.send(`[E-GDM] ${message.author.username}: ${message.content}`);
+
+        if (message.attachments.size > 0) {
+            message.attachments.forEach(attachment => {
+                forwardedMessage.channel.send(attachment.url);
+            });
         }
     }
 
     if (message.channel.id == config.groupIDs[1]) {
-        if (gdmOne && gdmOne.send) {
-            gdmOne.send(`[E-GDM] ${message.author.username}: ${message.content}`);
+        const forwardedMessage = await gdmOne.send(`[E-GDM] ${message.author.username}: ${message.content}`);
+
+        if (message.attachments.size > 0) {
+            message.attachments.forEach(attachment => {
+                forwardedMessage.channel.send(attachment.url);
+            });
         }
     }
 });
 
-client.login(process.env.token);
+async function stop() {
+    console.log('[E-GDM] Stopped');
+    await gdmOne.send('[E-GDM] stopping') && await gdmTwo.send('[E-GDM] stopping');
+}
+
+client.login(config.token);
+
+process.on('SIGINT', async () => {
+    await stop();
+    process.exit();
+});
+
+process.on('SIGTERM', async () => {
+    await stop();
+    process.exit();
+});
